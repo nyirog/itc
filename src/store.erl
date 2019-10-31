df70b626f3d893d4ef89abaa3650b95a959783c9
Nyiro Gergo <gergo.nyiro@gmail.com>
2019-10-31 23:13:18 +0100

    doc: add event store example

--- src/store.erl
+++ src/store.erl
@@ -1,7 +1,7 @@
 -module(store).
 
--export([init/0, append/2, fork/1, update/2]).
--export([get_last_seen_event_clock/2, list_unseen_events/2]).
+-export([init/0, append/2, fork/1, merge/2]).
+-export([get_last_seen_event_tic/2, list_unseen_events/2, get_last_tic/1]).
 
 -type action() :: any().
 -type tic() :: itc:itc().
@@ -10,11 +10,11 @@
 
 -spec list_unseen_events(events(), tic()) -> list(event()).
 list_unseen_events(Events, Tic) ->
-    Cmp = fun ({Toc, _}, {Tuc, _}) -> itc:leq(Tuc, Toc) end,
+    Cmp = fun ({A, _}, {B, _}) -> itc:leq(B, A) end,
     lists:sort(Cmp, [Event || {EventTic, _} = Event <- Events, not itc:leq(EventTic, Tic)]).
 
--spec get_last_seen_event_clock(events(), tic()) -> tic().
-get_last_seen_event_clock(Events, Tic) ->
+-spec get_last_seen_event_tic(events(), tic()) -> tic().
+get_last_seen_event_tic(Events, Tic) ->
     Tics = [EventTic || {EventTic, _} <- Events, itc:leq(EventTic, Tic)],
     Cmp = fun (A, B) -> itc:leq(B, A) end,
     case Tics of
@@ -27,16 +27,19 @@ get_last_seen_event_clock(Events, Tic) ->
 init() -> [{itc:seed(), init}].
 
 -spec append(events(), action()) -> events().
-append([{Tic, _} | _] = Events, Action) -> [{itc:event(Tic), Action}|Events].
+append(Events, Action) ->
+    [{itc:event(get_last_tic(Events)), Action}|Events].
 
 -spec fork(events()) -> list(events()).
-fork([{Tic, _} | _] = Events) ->
-    [Toc, ForkedToc] = itc:fork(Tic),
-    [[{Toc, fork} | Events], [{ForkedToc, fork}| Events]].
+fork(Events) ->
+    [Tic, Toc] = itc:fork(get_last_tic(Events)),
+    [[{Tic, fork} | Events], [{Toc, fork}| Events]].
 
+-spec merge(events(), list(event())) -> events().
+merge(Events, []) -> Events;
+merge(Events, UnseenEvents) ->
+    Tic = itc:event(itc:join(get_last_tic(Events), get_last_tic(UnseenEvents))),
+    [{Tic, merge} | UnseenEvents] ++ Events.
 
--spec update(events(), list(event())) -> events().
-update(Events, []) -> Events;
-update([{Tic, _} | _] = Events, [{Toc, _} | _] = NewEvents) ->
-    UpdateTic = itc:event(itc:join(Tic, Toc)),
-    [{UpdateTic, update} | NewEvents] ++ Events.
+-spec get_last_tic(events()) -> tic().
+get_last_tic([{Tic, _} | _]) -> Tic.
